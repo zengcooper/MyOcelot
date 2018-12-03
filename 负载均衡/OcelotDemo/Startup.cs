7cfddc1efb65601e2d88e1cb9574537c0eba3c0c
曾加维 <zengjw006045@rich-healthcare.com>
2018-12-03 12:32:57 +0800

    api网管 负载均衡实现

---
+++ 负载均衡/OcelotDemo/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.ConfigAuthLimitCache.DependencyInjection;
+using Ocelot.ConfigAuthLimitCache.Middleware;
+
+namespace OcelotDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //var authenticationProviderKey = "OcelotKey";
+            //var identityServerOptions = new IdentityServerOptions();
+            //Configuration.Bind("IdentityServerOptions", identityServerOptions);
+            //services.AddAuthentication(identityServerOptions.IdentityScheme)
+            //    .AddIdentityServerAuthentication(authenticationProviderKey, options =>
+            //    {
+            //        options.RequireHttpsMetadata = false; //是否启用https
+            //        options.Authority = $"http://{identityServerOptions.ServerIP}:{identityServerOptions.ServerPort}";//配置授权认证的地址
+            //        options.ApiName = identityServerOptions.ResourceName; //资源名称，跟认证服务中注册的资源列表名称中的apiResource一致
+            //        options.SupportedTokens = SupportedTokens.Both;
+            //    }
+            //    );
+            //services.AddOcelot()//注入Ocelot服务
+            //        .AddAuthLimitCache(option=> {
+            //            option.DbConnectionStrings = "Server=.;Database=Ocelot;User ID=sa;Password=123;";
+            //        })
+            //        .AddConsul();
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddOcelot();//注入Ocelot服务
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            ////app.UseAuthentication();
+            //app.UseAhphOcelot().Wait();
+            ////app.UseOcelot().Wait();//使用Ocelot中间件
+            ////app.UseMvc();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseOcelot().Wait();//使用Ocelot中间件
+            app.UseMvc();
+        }
+    }
+}
